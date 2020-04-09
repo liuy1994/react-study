57699ed1ce6e0ecbc0a32a902b5becaf9118d003
liuy1994 <liuy1994g@gmail.com>
2020-04-09 10:24:13 +0800

    reset

--- first_react-app/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const FilterTypes = {
-    All: 'all',
-    COMPLETED: 'completed',
-    UNCOMPLETED: 'uncompleted'
-}
--- first_react-app/src/views/todos/index.js
+++
@@ -1,4 +0,0 @@
-import Todos from './todos'
-
-console.log(Todos)
-export { Todos }
